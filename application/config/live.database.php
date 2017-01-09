5cd630e1004d9285deedf5f8343cdaa6ff377af9
Korvin Szanto <korvinszanto@gmail.com>
2017-01-09 14:41:23 -0800

    Add live database config

---
+++ application/config/live.database.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * dotenv based db configuration
+ */
+return array(
+    'default-connection' => 'live',
+    'connections' => array(
+        'wercker' => array(
+            'driver' => 'c5_pdo_mysql',
+            'server' => getenv('DB_SERVER'),
+            'database' => getenv('DB_DATABASE'),
+            'username' => getenv('DB_USER'),
+            'password' => getenv('DB_PASSWORD'),
+            'charset' => 'utf8mb4',
+        ),
+    ),
+);
