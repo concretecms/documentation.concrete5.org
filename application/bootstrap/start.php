a76eb3b1799aaf13fd5ab164243b4e9c5ccf43f1
Andrew Embler <andrew@concrete5.org>
2019-06-01 14:24:01 +0000

    updating the code we use for dotenv

--- application/bootstrap/start.php
+++ application/bootstrap/start.php
@@ -3,16 +3,6 @@
 use Concrete\Core\Application\Application;
 
 /**
- * Load in a .env if there is one
- */
-try {
-    $dotenv = new Dotenv\Dotenv(__DIR__ . "/../../");
-    $dotenv->load();
-} catch (\Exception $e) {
-    // Ignore exception
-}
-
-/**
  * ----------------------------------------------------------------------------
  * Instantiate concrete5
  * ----------------------------------------------------------------------------
@@ -24,14 +14,5 @@ $app = new Application();
  * Detect the environment based on the hostname of the server
  * ----------------------------------------------------------------------------
  */
-$app->detectEnvironment(
-    array(
-        'andrew' => array(
-            'Andrews-MacBook-Pro.local'
-        ),
-        'live' => array(
-            'documentation.concrete5.org'
-        )
-    ));
-
+$app->detectEnvironment([]);
 return $app;
